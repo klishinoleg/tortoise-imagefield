0377775110312974c86ed2af73faeb7fb0776ceb
klishinoleg <klishinoleg@gmail.com>
2025-04-02 16:57:13 +0300

    V 0.1.4 - Remove boto3 from requires

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tortoise-imagefield",
-    version="0.1.3",
+    version="0.1.4",
     author="Klishin Oleg",
     author_email="klishinoleg@gmail.com",
     description="Asynchronous Tortoise ORM field for handling image uploads with cropping, caching, and support for AWS S3 or local storage.",
@@ -12,7 +12,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         "tortoise-orm",
-        "boto3",
         "pillow",
         "starlette",
         "asgiref",
